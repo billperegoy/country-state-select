8444f348de91af6f0c035915ee7b0adbb99845b3
Bill Peregoy <bill.peregoy@endurance.com>
2017-05-25 20:56:04 -0400

    Added tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,17 +2,77 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
-import Utils
+import Country exposing (..)
 
 
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Capitalizes a lower case word" <|
+        [ test "Handles empty lists" <|
             \() ->
-                Expect.equal (Utils.capitalize "john") "John"
-        , test "Does not change an already capitalized word" <|
+                let
+                    pinned =
+                        []
+
+                    countries =
+                        []
+                in
+                    Expect.equal (Country.pinAndSort pinned countries) []
+        , test "Handles empty pinned list" <|
             \() ->
-                Expect.equal (Utils.capitalize "John") "John"
+                let
+                    pinned =
+                        []
+
+                    countries =
+                        [ Country "us" "United States"
+                        , Country "ca" "Canada"
+                        ]
+
+                    sortedCountries =
+                        [ Country "ca" "Canada"
+                        , Country "us" "United States"
+                        ]
+                in
+                    Expect.equal (Country.pinAndSort pinned countries) sortedCountries
+        , test "Handles single pinned element" <|
+            \() ->
+                let
+                    pinned =
+                        [ "mx" ]
+
+                    countries =
+                        [ Country "us" "United States"
+                        , Country "mx" "Mexico"
+                        , Country "ca" "Canada"
+                        ]
+
+                    sortedCountries =
+                        [ Country "mx" "Mexico"
+                        , Country "ca" "Canada"
+                        , Country "us" "United States"
+                        ]
+                in
+                    Expect.equal (Country.pinAndSort pinned countries) sortedCountries
+        , test "Handles multiple pinned elements" <|
+            \() ->
+                let
+                    pinned =
+                        [ "us", "ca" ]
+
+                    countries =
+                        [ Country "mx" "Mexico"
+                        , Country "ba" "Bahamas"
+                        , Country "ca" "Canada"
+                        , Country "us" "United States"
+                        ]
+
+                    sortedCountries =
+                        [ Country "us" "United States"
+                        , Country "ca" "Canada"
+                        , Country "ba" "Bahamas"
+                        , Country "mx" "Mexico"
+                        ]
+                in
+                    Expect.equal (Country.pinAndSort pinned countries) sortedCountries
         ]
